7521aee06c6b7e690520db03ac1dac11f4f89abb
deptoeycc <deptoeycc@gmail.com>
2026-01-29 17:10:24 -0300

    update 29-01

---
+++ statistics/statistics.py
@@ -0,0 +1,16 @@
+from preliminar.stack_ranges import stack_ranges
+
+def climg_mean(ds_sst, MHW_date, climY_start, climY_end, MHW_window = 5):
+    
+    return stack_ranges(ds_sst, MHW_date, climY_start, climY_end, MHW_window).mean()
+
+def climg_thresh(ds_sst, MHW_date, climY_start, climY_end, MHW_window = 5, thresh = 90):
+    
+    return stack_ranges(ds_sst, MHW_date, climY_start, climY_end, MHW_window)..quantile(thresh/100, dim="time")
+
+def climg_diff(ds_sst, MHW_date, climY_start, climY_end, MHW_window = 5, thresh = 90):
+
+    mean = climg_mean(ds_sst, MHW_date, climY_start, climY_end, MHW_window = 5)
+    thresh = climg_thresh(ds_sst, MHW_date, climY_start, climY_end, MHW_window = 5, thresh = 90)
+
+    
